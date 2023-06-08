43b4767bb1e0444cf01af5702f7e663eb786e9e8
alesya <laykovichao@gmail.com>
2023-06-08 14:00:10 +0300

    added 5 files for h/w_1

---
+++ task_1.py
@@ -0,0 +1,14 @@
+# Дана длина ребра куба. Найти объем куба и площадь его боковой поверхности.
+
+length = 6
+V = length ** 3
+S_side = 4 * length ** 2
+print(V)
+print(S_side)
+
+# если вводим данные с клавиатуры, то:
+length = int(input('Введите длину ребра: '))
+V = length ** 3
+S_side = 4 * length ** 2
+print(V)
+print(S_side)
